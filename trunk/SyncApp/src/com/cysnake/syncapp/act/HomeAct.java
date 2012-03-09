dc18c03b687ae9dbb081b9511ce97c4dd8912736
matt.cai <cysnake4713@gmail.com>
2012-03-09 04:41:32 -0430

    as

---
+++ trunk/SyncApp/src/com/cysnake/syncapp/act/HomeAct.java
@@ -0,0 +1,14 @@
+package com.cysnake.syncapp.act;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeAct extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+
+	}
+}
