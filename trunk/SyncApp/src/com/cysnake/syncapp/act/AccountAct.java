3213d37cfe81e31171bdce6c70d4d50378e22622
matt.cai <cysnake4713@gmail.com>
2012-03-16 03:43:05 -0430

    this one is really bad ass

---
+++ trunk/SyncApp/src/com/cysnake/syncapp/act/AccountAct.java
@@ -0,0 +1,14 @@
+package com.cysnake.syncapp.act;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountAct extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account);
+	}
+
+}
